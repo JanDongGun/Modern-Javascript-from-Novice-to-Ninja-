cbbf281e9e8cecad2d18474269fa24673a032f32
jandonggun <donggontrannguyen@gmail.com>
2021-02-08 08:36:31 +0700

    complete funtion & method

---
+++ 4.FunctionAndMethod/app.js
@@ -0,0 +1,16 @@
+// // function declaration
+// function greet() {
+//   console.log("hello there");
+// }
+
+// greet();
+
+// // function expression
+
+// const speak = function () {
+//   console.log("hello 2");
+// };
+
+// speak();
+
+// // hoisting work with  function declaration not function expression
