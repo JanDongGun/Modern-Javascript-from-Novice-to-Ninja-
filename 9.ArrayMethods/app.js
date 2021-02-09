94556193da0d06dbbd3d6fd61b6cf6769c04105e
jandonggun <donggontrannguyen@gmail.com>
2021-02-09 09:57:50 +0700

    map method

--- 9.ArrayMethods/app.js
+++ 9.ArrayMethods/app.js
@@ -9,3 +9,22 @@
 
 // let filterUser = users.filter((user) => user.age > 20);
 // console.log(filterUser);
+
+// Map method
+
+// const users = [
+//   { name: "gon", age: 21 },
+//   { name: "tran", age: 21 },
+//   { name: "gontan", age: 18 },
+//   { name: "trangon", age: 18 },
+// ];
+
+// const mapUsers = users.map((user) => {
+//   if (user.age < 20) {
+//     return { name: user.name, age: user.age / 2 };
+//   } else {
+//     return user;
+//   }
+// });
+
+// console.log(mapUsers, users);
