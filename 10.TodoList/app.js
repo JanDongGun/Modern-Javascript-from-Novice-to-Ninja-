b80488911901aca83567c169402cb51aeb8c0cf9
jandonggun <donggontrannguyen@gmail.com>
2021-02-09 16:46:32 +0700

    filter todo

--- 10.TodoList/app.js
+++ 10.TodoList/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 const generateTemplate = (todo) => {
   const html = `
@@ -28,4 +29,22 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+// keyup event
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+
+  filterTodos(term);
 });
