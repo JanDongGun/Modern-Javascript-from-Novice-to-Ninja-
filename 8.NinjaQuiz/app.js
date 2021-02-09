26ac64262653dd11c830271c73b040aab5b1e21f
jandonggun <donggontrannguyen@gmail.com>
2021-02-09 09:07:36 +0700

    checking answers

---
+++ 8.NinjaQuiz/app.js
@@ -0,0 +1,23 @@
+const correctAnswers = ["B", "B", "B", "B"];
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+
+  let userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
