c86824fb6fa1b5b776b89f729ee7c16df67cecd9
jandonggun <donggontrannguyen@gmail.com>
2021-02-09 09:51:19 +0700

    filter method

---
+++ 9.ArrayMethods/app.js
@@ -0,0 +1,11 @@
+// Filter Method
+
+// const users = [
+//   { name: "gon", age: 21 },
+//   { name: "tran", age: 21 },
+//   { name: "gontan", age: 18 },
+//   { name: "trangon", age: 18 },
+// ];
+
+// let filterUser = users.filter((user) => user.age > 20);
+// console.log(filterUser);
