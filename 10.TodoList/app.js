6a68b663a62d7812a2966b38e615e4b998cfa297
jandonggun <donggontrannguyen@gmail.com>
2021-02-09 16:22:10 +0700

    remove todos

--- 10.TodoList/app.js
+++ 10.TodoList/app.js
@@ -21,3 +21,11 @@ addForm.addEventListener("submit", (e) => {
     addForm.reset();
   }
 });
+
+// delete todos
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
