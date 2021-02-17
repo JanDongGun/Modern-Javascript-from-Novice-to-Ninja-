63c1549bd4093c1a7101c9bc556b1242925691fa
jandonggun <donggontrannguyen@gmail.com>
2021-02-17 10:00:15 +0700

    complete date time'

--- 11.DatesTimes/app.js
+++ 11.DatesTimes/app.js
@@ -32,22 +32,22 @@
 
 // console.log(mins, hours, days);
 
-const clock = document.querySelector(".clock");
+// const clock = document.querySelector(".clock");
 
-const tick = () => {
-  const now = new Date();
+// const tick = () => {
+//   const now = new Date();
 
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
+//   const h = now.getHours();
+//   const m = now.getMinutes();
+//   const s = now.getSeconds();
 
-  const html = `
-    <span>${h} : </span>
-    <span>${m} : </span>
-    <span>${s}</span>
-  `;
+//   const html = `
+//     <span>${h} : </span>
+//     <span>${m} : </span>
+//     <span>${s}</span>
+//   `;
 
-  clock.innerHTML = html;
-};
+//   clock.innerHTML = html;
+// };
 
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
