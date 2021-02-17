68b2f32d340b78cc34e7508835a338e85ff0eac2
jandonggun <donggontrannguyen@gmail.com>
2021-02-17 17:07:42 +0700

    weather icon

--- 13.WeatherApp/scripts/app.js
+++ 13.WeatherApp/scripts/app.js
@@ -1,10 +1,11 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector(".time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-  const cityDets = data.cityDets;
-  const weather = data.weather;
+  const { cityDets, weather } = data;
 
   // update details template
 
@@ -16,6 +17,19 @@ const updateUI = (data) => {
     <span>&deg;</span>
   </div>
   `;
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+
+  time.setAttribute("src", timeSrc);
 
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
