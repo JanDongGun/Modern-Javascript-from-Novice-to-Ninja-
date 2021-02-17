ee5cf27ce62190f2d450b3c944c22ddfff0a8ac8
jandonggun <donggontrannguyen@gmail.com>
2021-02-17 09:36:52 +0700

    comparisons

--- 11.Dates & Times/app.js
+++ 11.Dates & Times/app.js
@@ -1,20 +1,33 @@
 // dates & times
 
-const now = new Date();
-console.log(now);
+// const now = new Date();
+// console.log(now);
 
 // year, month, day, times
 
-console.log("getFullYear", now.getFullYear());
-console.log("getMonth", now.getMonth());
-console.log("getDate", now.getDate());
-console.log("getDay", now.getDay());
+// console.log("getFullYear", now.getFullYear());
+// console.log("getMonth", now.getMonth());
+// console.log("getDate", now.getDate());
+// console.log("getDay", now.getDay());
 
 // timestamps
 
-console.log("timestamp", now.getTime());
+// console.log("timestamp", now.getTime());
 
 // date strins
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+
+// Comparisons
+
+const before = new Date("February 1 2019 7:30:59");
+const now = new Date();
+
+const diff = now.getTime() - before.getTime();
+
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
+
+console.log(mins, hours, days);
