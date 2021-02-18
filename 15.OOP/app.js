493c09457de1675b0fd7c276a476ed76929b80ba
jandonggun <donggontrannguyen@gmail.com>
2021-02-18 10:54:22 +0700

    class method

--- 15.OOP/app.js
+++ 15.OOP/app.js
@@ -6,9 +6,26 @@ class User {
   constructor(name, email) {
     this.name = name;
     this.email = email;
+    this.score = 0;
+  }
+
+  login() {
+    console.log(`${this.name} just logged in`);
+    return this;
+  }
+
+  logout() {
+    console.log(`${this.name} just logged out`);
+    return this;
+  }
+
+  incScore() {
+    this.score += 1;
+    console.log(`${this.name} has a score of ${this.score}`);
+    return this;
   }
 }
 
 const userOne = new User("gon", "trannguyendonggon@gmail.com");
 
-console.log(userOne);
+userOne.login().incScore().incScore().logout();
