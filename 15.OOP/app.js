2d06ad0344c5b2077d2a27911582e22438728569
jandonggun <donggontrannguyen@gmail.com>
2021-02-19 09:55:57 +0700

    constructor

--- 15.OOP/app.js
+++ 15.OOP/app.js
@@ -2,43 +2,51 @@
 
 // console.log(typeof age);
 
-class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-    this.score = 0;
-  }
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//     this.score = 0;
+//   }
 
-  login() {
-    console.log(`${this.name} just logged in`);
-    return this;
-  }
+//   login() {
+//     console.log(`${this.name} just logged in`);
+//     return this;
+//   }
 
-  logout() {
-    console.log(`${this.name} just logged out`);
-    return this;
-  }
+//   logout() {
+//     console.log(`${this.name} just logged out`);
+//     return this;
+//   }
 
-  incScore() {
-    this.score += 1;
-    console.log(`${this.name} has a score of ${this.score}`);
-    return this;
-  }
-}
+//   incScore() {
+//     this.score += 1;
+//     console.log(`${this.name} has a score of ${this.score}`);
+//     return this;
+//   }
+// }
 
-class Admin extends User {
-  constructor(name, email, title) {
-    super(name, email);
-    this.title = title;
-  }
-  deleteUser(user) {
-    users = users.filter((u) => u.name != user.name);
-  }
+// class Admin extends User {
+//   constructor(name, email, title) {
+//     super(name, email);
+//     this.title = title;
+//   }
+//   deleteUser(user) {
+//     users = users.filter((u) => u.name != user.name);
+//   }
+// }
+
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+  this.login = function () {
+    console.log(`${this.name} has logged in`);
+  };
 }
 
 const userOne = new User("gon", "trannguyendonggon@gmail.com");
 const userTwo = new User("tran", "trannguyendonggon@gmail.com");
-const userThree = new Admin("admin", "trannguyendonggon@gmail.com", "ninja");
+//const userThree = new Admin("admin", "trannguyendonggon@gmail.com", "ninja");
 
-let users = [userOne, userTwo, userThree];
-console.log(userTwo);
+//let users = [userOne, userTwo, userThree];
+console.log(userTwo.login);
