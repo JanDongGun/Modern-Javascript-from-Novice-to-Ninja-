af08829efecbc29fec15e58daba3e44908133e2f
jandonggun <donggontrannguyen@gmail.com>
2021-02-20 08:59:21 +0700

    add new chat

--- 17.Chatroom/scripts/app.js
+++ 17.Chatroom/scripts/app.js
@@ -1,5 +1,18 @@
 // DOM queries
 const chatList = document.querySelector(".chat-list");
+const newChat = document.querySelector(".new-chat");
+
+// add a new chat
+newChat.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = newChat.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => {
+      newChat.reset();
+    })
+    .catch((err) => console.log(err));
+});
 
 // class instances
 const chatroom = new Chatroom("general", "gon");
