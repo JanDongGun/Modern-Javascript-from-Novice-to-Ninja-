c4eab47372c53eeecaa837fa30d576b21a6ae583
jandonggun <donggontrannguyen@gmail.com>
2021-02-20 09:42:54 +0700

    changing username

--- 17.Chatroom/scripts/app.js
+++ 17.Chatroom/scripts/app.js
@@ -1,7 +1,8 @@
 // DOM queries
 const chatList = document.querySelector(".chat-list");
 const newChat = document.querySelector(".new-chat");
-
+const newName = document.querySelector(".new-name");
+const updateMssg = document.querySelector(".update-mssg");
 // add a new chat
 newChat.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -14,8 +15,25 @@ newChat.addEventListener("submit", (e) => {
     .catch((err) => console.log(err));
 });
 
+// update username
+newName.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // update name via chatroom
+  const name = newName.name.value.trim();
+  chatroom.updateName(name);
+  // reset the from
+  newName.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${name}`;
+  setTimeout(() => {
+    updateMssg.innerText = "";
+  }, 2000);
+});
+
+// check local storage
+const username = localStorage.username ? localStorage.username : "anon";
 // class instances
-const chatroom = new Chatroom("general", "gon");
+const chatroom = new Chatroom("general", username);
 const chatUI = new ChatUI(chatList);
 // get chats and render
 chatroom.getChats((data) => {
--- 17.Chatroom/scripts/chat.js
+++ 17.Chatroom/scripts/chat.js
@@ -41,6 +41,7 @@ class Chatroom {
 
   updateName(username) {
     this.username = username;
+    localStorage.setItem("username", username);
   }
 
   updateRoom(room) {
