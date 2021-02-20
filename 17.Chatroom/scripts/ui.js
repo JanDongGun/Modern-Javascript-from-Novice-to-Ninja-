1fb7dcb467f8280db8d4799b7defedbc2cc7e200
jandonggun <donggontrannguyen@gmail.com>
2021-02-20 08:52:06 +0700

    format date

--- 17.Chatroom/scripts/ui.js
+++ 17.Chatroom/scripts/ui.js
@@ -7,11 +7,14 @@ class ChatUI {
   }
 
   render(data) {
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true,
+    });
     const html = `
         <li class="list-group-item">
-            <span class="username>${data.username}</span>
-            <span class="message>${data.message}</span>
-            <div class="time">${data.created_at.toDate()}</div>
+            <span class="username">${data.username}</span>
+            <span class="message">${data.message}</span>
+            <div class="time">${when}</div>
         </li>
       `;
 
