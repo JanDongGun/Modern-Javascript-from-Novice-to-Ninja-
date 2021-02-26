56ea7f435fb3a654e47d1f56c349b1bf05976a62
jandonggun <donggontrannguyen@gmail.com>
2021-02-26 18:41:05 +0700

    default exports

---
+++ 19.BabelWebpack/src/data.js
@@ -0,0 +1,9 @@
+const users = [
+  { name: "mario", premium: true },
+  { name: "luigi", premium: false },
+  { name: "yoshi", premium: false },
+  { name: "toad", premium: true },
+  { name: "peach", premium: false },
+];
+
+export default users;
--- 19.BabelWebpack/src/index.js
+++ 19.BabelWebpack/src/index.js
@@ -1,5 +1,8 @@
 import { styleBody, addTitle } from "./dom";
+import users from "./data";
 
 console.log("index file");
 addTitle("test");
 styleBody();
+
+console.log(users);
