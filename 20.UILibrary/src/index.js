579d605039f831950d88d219688edf035adf1de3
jandonggun <donggontrannguyen@gmail.com>
2021-02-27 16:51:42 +0700

    snackbar js

--- 20.UILibrary/src/index.js
+++ 20.UILibrary/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from "./ninja-ui/tooltip";
 import Dropdown from "./ninja-ui/dropdown";
 import Tabs from "./ninja-ui/tabs";
+import Snackbar from "./ninja-ui/snackbar";
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
@@ -15,3 +16,12 @@ dropdowns.forEach((dropdown) => {
 // create tabs
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  snackbar.show("hello my friend, have a nice day");
+});
