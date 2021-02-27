8b9ef09bf93afad456b46629c5b9e57ebde334ef
jandonggun <donggontrannguyen@gmail.com>
2021-02-27 08:17:45 +0700

    init project ui library

---
+++ 20.UILibrary/src/index.js
@@ -0,0 +1,17 @@
+import { styleBody, addTitle } from "./dom";
+import users from "./data";
+
+console.log("index file");
+addTitle("test");
+styleBody();
+
+const donggon = (gon) => {
+  console.log(gon);
+};
+
+const donggon1 = 1;
+
+donggon("gon");
+
+console.log(users);
+console.log("hello gon");
