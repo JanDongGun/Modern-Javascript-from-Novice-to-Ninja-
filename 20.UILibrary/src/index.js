8da0e145b41456b29b3a941e5d1f2c9f14b10fbf
jandonggun <donggontrannguyen@gmail.com>
2021-02-27 11:02:08 +0700

    dropdown

--- 20.UILibrary/src/index.js
+++ 20.UILibrary/src/index.js
@@ -1,5 +1,13 @@
 import Tooltip from "./ninja-ui/tooltip";
+import Dropdown from "./ninja-ui/dropdown";
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
+
+// create a dropdown
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
